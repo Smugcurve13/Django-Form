21f9fd12e978e9d21c791898db35a4894d98c62f
Smugcurve13 <sambhavsoni14@gmail.com>
2024-11-05 10:58:51 +0530

    can send mail upon submission

--- job_application/views.py
+++ job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 # Create your views here.
 
 def index(request):
@@ -15,6 +16,10 @@ def index(request):
             occupation = form.cleaned_data['occupation']
 
             Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date, occupation=occupation)
+
+            message_body = f"A new job application was submitted. Thank You, {first_name}"
+            email_message = EmailMessage("Form Submission Confirmation", message_body, to=[email])
+            email_message.send()
 
             messages.success(request, "Form Submitted Successfully")
     return render(request, "index.html")
