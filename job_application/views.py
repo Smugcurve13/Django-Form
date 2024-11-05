cf97b906220a0cf7a726e64249a5edcfc02b695c
Smugcurve13 <sambhavsoni14@gmail.com>
2024-11-05 10:18:22 +0530

    data is now stored in database and success message is shown

--- job_application/views.py
+++ job_application/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -11,4 +13,8 @@ def index(request):
             email = form.cleaned_data['email']
             date = form.cleaned_data['date']
             occupation = form.cleaned_data['occupation']
+
+            Form.objects.create(first_name=first_name, last_name=last_name, email=email, date=date, occupation=occupation)
+
+            messages.success(request, "Form Submitted Successfully")
     return render(request, "index.html")
